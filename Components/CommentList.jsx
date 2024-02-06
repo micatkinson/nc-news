0a0246bdf6c5305c210c5a9d343df252d69caa5b
micatkinson <147393108+micatkinson@users.noreply.github.com>
2024-02-06 10:55:21 +0000

    add basic stack layout for comments

--- Components/CommentList.jsx
+++ Components/CommentList.jsx
@@ -1,14 +1,18 @@
+import Stack from 'react-bootstrap/Stack';
+
 export default function CommentList({comments}){
     return (
     <section>
         <h2>Comments</h2>
         <ul>
             {comments.map((comment) => (
-                <div key={[comment.id, 'div']} className="comments">
+                <Stack gap={3}>
+                <div key={[comment.id, 'div']} className="p-2">
                 <h4 key={[comment.id, 'title']}>{comment.author}</h4>
                 <p key={[comment.id, 'p']}>{comment.body}</p>
-                <li key={[comment.id, 'li']}>{comment.votes}</li>
+                <li key={[comment.id, 'li']}>Votes: {comment.votes}</li>
                 </div>
+                </Stack>
             ))}
         </ul>
     </section>
