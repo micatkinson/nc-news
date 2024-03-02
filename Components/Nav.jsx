1cce49a37d39a4c732369d5fe22b214b4c5fcfa3
micatkinson <147393108+micatkinson@users.noreply.github.com>
2024-03-02 19:57:10 +0000

    update styling

--- Components/Nav.jsx
+++ Components/Nav.jsx
@@ -4,7 +4,6 @@ import { useContext } from "react"
 
 export default function Nav(){
     const { loggedInUser } = useContext(UserContext)
-
     return (
         <>
         <nav className='navBar'>
